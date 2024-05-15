947d50726c2942a2af5fef0b1820fe52de9e106c
Gilvarsan <gilvarsan@gmail.com>
2024-05-14 20:37:33 -0500

    Se adciona regexs

--- funciones.js
+++ funciones.js
@@ -1,10 +1,13 @@
 const validarFecha = (date) => {
-  let dateAux;
   if (!date) {
-    dateAux = new Date();
-  } else if (date.length === 13) {
-    dateAux = new Date(parseInt(date));
-  } else if (date.length === 10) {
+    return new Date();
+  }
+  let dateAux;
+  const regexs = [/^\d+$/, /\d{3}-\d{2}-\d{2}/];
+
+  if (regexs[0].test(date)) {
+    dateAux = new Date(Number(date));
+  } else if (regexs[1].test(date)) {
     dateAux = new Date(date);
   } else {
     dateAux = 0;
